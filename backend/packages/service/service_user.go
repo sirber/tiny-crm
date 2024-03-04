d77b2109c4e41d9b6d354c7737ff21e1082011f2
Stephane Berube <sirber@protonmail.com>
2024-03-03 20:31:47 -0500

    added: UpdateUser, CreateUser

--- backend/packages/service/service_user.go
+++ backend/packages/service/service_user.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"main/packages/database"
 	"main/packages/security"
 
@@ -23,15 +24,45 @@ func GetUserByToken(token string) (*database.User, error) {
 	return repository.GetUserByToken(token)
 }
 
+func CreateUser(user *database.User) error {
+	if user.ID != uuid.Nil {
+		return errors.New("new user cannot have an ID set")
+	}
+
+	hashedPassword, err := security.HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+
+	user.Password = hashedPassword
+
+	return repository.CreateUser(user)
+}
+
 func UpdateUser(user *database.User) error {
-	if user.Password != "" { // FIXME: use a DTO, convert to User
+	if user.ID == uuid.Nil {
+		return errors.New("updated user must have an ID set")
+	}
+
+	currentUser, err := GetUser(user.ID)
+	if err != nil {
+		return err
+	}
+
+	if user.Password != "" {
 		hashedPassword, err := security.HashPassword(user.Password)
 		if err != nil {
 			return err
 		}
 
 		user.Password = hashedPassword
+	} else {
+		user.Password = currentUser.Password
 	}
+
+	user.Token = currentUser.Token
+	user.CreatedAt = currentUser.CreatedAt
+	user.UpdatedAt = currentUser.UpdatedAt
 
 	return repository.UpdateUser(user)
 }
