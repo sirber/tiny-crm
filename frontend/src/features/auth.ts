9319828f7d96ba50af4d36fb5b82eaef7815084e
Stephane Berube <sirber@protonmail.com>
2024-04-25 08:33:39 -0400

    added: prettier cleanup

--- frontend/src/features/auth.ts
+++ frontend/src/features/auth.ts
@@ -1,73 +1,76 @@
-import { Dispatch, createSlice } from "@reduxjs/toolkit";
-import { RootState } from "../store"; // assuming you have a RootState type defined
+import { Dispatch, createSlice } from '@reduxjs/toolkit'
+import { RootState } from '../store'
 
 // Define the interface for the state
 interface AuthState {
-  isAuthenticated: boolean;
+  isAuthenticated: boolean
 }
 
 // Initial state
 const initialState: AuthState = {
-  isAuthenticated: false,
-};
+  isAuthenticated: false
+}
 
 // Create the slice
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     loginSuccess(state) {
-      state.isAuthenticated = true;
+      state.isAuthenticated = true
     },
     logoutSuccess(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+      state.isAuthenticated = false
+    }
+  }
+})
 
 // Export actions
-export const { loginSuccess, logoutSuccess } = authSlice.actions;
+export const { loginSuccess, logoutSuccess } = authSlice.actions
 
 // Export reducer
-export default authSlice.reducer;
+export default authSlice.reducer
 
 // Selectors
 export const selectIsAuthenticated = (state: RootState) =>
-  state.auth.isAuthenticated;
+  state.auth.isAuthenticated
 
 // Thunks
 export const login =
   (username: string, password: string) => async (dispatch: Dispatch) => {
-    console.log(username, password);
-
-    // Perform login logic here, like making an API call
-    // For example:
     try {
-      // Simulate API call
+      console.log(username, password)
+
+      // TODO: API call
       // const response = await api.login(username, password);
       // const data = response.data;
 
-      // Simulated success
-      dispatch(loginSuccess());
+      dispatch(loginSuccess())
     } catch (error) {
-      // Handle login failure
-      console.error("Login failed:", error);
-      // You might want to dispatch an action here to update state indicating login failure
+      // TODO: handle error
+      console.error('Login failed:', error)
     }
-  };
+  }
 
 export const logout = () => async (dispatch: Dispatch) => {
-  // Perform logout logic here, like making an API call to invalidate session
-  // For example:
   try {
-    // Simulate API call
+    // TODO: API call
     // const response = await api.logout();
 
-    // Simulated success
-    dispatch(logoutSuccess());
+    dispatch(logoutSuccess())
   } catch (error) {
-    // Handle logout failure
-    console.error("Logout failed:", error);
-    // You might want to dispatch an action here to update state indicating logout failure
+    // TODO: handle error
+    console.error('Logout failed:', error)
   }
-};
+}
+
+export const session = () => async (dispatch: Dispatch) => {
+  try {
+    // TODO: API call
+
+    dispatch(loginSuccess())
+  } catch (error) {
+    // TODO: handle error
+    console.error('Session check failed:', error)
+  }
+}
