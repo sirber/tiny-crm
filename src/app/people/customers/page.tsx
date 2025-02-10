8680ce39dc3bb8791d374b06335a53e150297987
Stephane Berube <sirber83@gmail.com>
2025-02-09 19:29:12 -0500

    added: database lib

--- src/app/people/customers/page.tsx
+++ src/app/people/customers/page.tsx
@@ -1,8 +1,6 @@
-import { PrismaClient } from "@prisma/client";
 import { getUser } from "@/lib/session";
 import { CustomerList } from "@/features/people/CustomerList";
-
-const prisma = new PrismaClient();
+import prisma from "@/lib/database";
 
 export default async function Customer() {
   const user = await getUser();
