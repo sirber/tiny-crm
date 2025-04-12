f54e5e32a1e74a076e326b4fd70ebed7da65af87
Stephane Berube <sirber83@gmail.com>
2025-04-11 22:18:47 -0400

    added: sample admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 import { getRole } from "@/lib/session";
 import { forbidden } from "next/navigation";
+import { Box, Paper, Typography } from "@mui/material";
 
 export default async function AdminPage() {
   const role = await getRole();
@@ -8,8 +9,42 @@ export default async function AdminPage() {
   }
 
   return (
-    <div>
-      <h1>Admin Page</h1>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        gap: "8px",
+        minHeight: "300px",
+      }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          width: "25%",
+          p: 2,
+          boxSizing: "border-box",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h6">Administration</Typography>
+        {/* Add left sidebar content here */}
+      </Paper>
+
+      {/* Right Pane (3/4 of the width) */}
+      <Paper
+        elevation={3}
+        sx={{
+          width: "75%",
+          p: 2,
+          boxSizing: "border-box",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h6">Users</Typography>
+        {/* Add main content here */}
+      </Paper>
+    </Box>
   );
 }
